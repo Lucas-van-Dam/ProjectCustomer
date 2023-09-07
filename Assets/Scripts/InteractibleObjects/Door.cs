88012b5c938f5aa700ddcf3f107134da9da43999
Jimmy1602 <aighter02@gmail.com>
2023-09-07 15:57:01 +0200

    door prefab

---
+++ Assets/Scripts/InteractibleObjects/Door.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    Transform hinge;
+
+    [SerializeField]
+    float rotationAngle = 80;
+
+    [SerializeField]
+    float rotationSpeed = 0.9f;
+
+    float baseRotation;
+    float targetRotation;
+
+
+    void Start()
+    {
+
+
+        baseRotation = hinge.rotation.y;
+        targetRotation = baseRotation;
+    }
+
+    public void Interact(GameObject interactor)
+    {
+        if (targetRotation == baseRotation)
+            targetRotation = baseRotation + rotationAngle;
+        else
+            targetRotation = baseRotation;
+    }
+
+    void FixedUpdate()
+    {
+        hinge.rotation = Quaternion.Lerp(hinge.rotation, new Quaternion(0, targetRotation, 0, 1), rotationSpeed);
+        Debug.Log(hinge.rotation);
+        Debug.Log(new Quaternion(0, targetRotation, 0, 1));
+        
+    }
+}
---
+++ Assets/Scripts/InteractibleObjects/PickUp.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour, IInteractable
+{
+    GameObject player;
+    Transform cameraTransform;
+    Rigidbody rb;
+
+    [SerializeField]
+    float hoverDistance = 5;
+    [SerializeField]
+    float hoverSpeed = 20;
+    [SerializeField]
+    float hoverDrag = 20;
+
+
+    bool isBeingHeld = false;
+
+    bool justPutDown = false;
+
+
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            if (Vector3.Distance(transform.position, GetHoverTargetPosition()) > 0.5f)
+            {
+                Vector3 moveDir = (GetHoverTargetPosition() - transform.position).normalized * hoverSpeed;
+
+                rb.AddForce(moveDir);
+            }
+
+
+            //transform.position = Vector3.Lerp(transform.position, GetHoverTargetPosition(), 0.1f);
+                
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(cameraTransform.position - transform.position, Vector3.up), 1);
+
+            //transform.rotation = Quaternion.AngleAxis(Vector3.Angle(player.transform.position, transform.position), Vector3.up);
+
+            //Debug.Log(Quaternion.AngleAxis(Vector3.Angle(player.transform.position, transform.position), Vector3.up));
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Drop();
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        justPutDown = false;
+    }
+
+
+    void PickingUp()
+    {
+        rb.useGravity = false;
+        rb.drag = hoverDrag;
+
+        isBeingHeld = true;
+
+        gameObject.layer = 2;
+
+    }
+
+    void Drop()
+    {
+        rb.useGravity = true;
+        rb.drag = 0;
+        isBeingHeld = false;
+        gameObject.layer = 0;
+        justPutDown = true;
+
+    }
+
+
+    public void Interact(GameObject interactor)
+    {
+        if (!justPutDown)
+        {
+            player = interactor;
+            cameraTransform = player.GetComponentInChildren<Camera>().transform;
+            PickingUp();
+        }
+    }
+
+
+    Vector3 GetHoverTargetPosition()
+    {
+        return player.transform.position + (cameraTransform.forward * hoverDistance);
+    }
+}
