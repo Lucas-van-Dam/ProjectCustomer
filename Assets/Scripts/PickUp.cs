8d7c0af0455e8814bf9e1019354fc9134d508e3a
Ciar <138235847+Lucas-van-Dam@users.noreply.github.com>
2023-09-07 14:49:50 +0200

    Update PickUp.cs

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -47,7 +47,7 @@ public class PickUp : MonoBehaviour, IInteractable
     }
 
 
-    public void Interact(GameObject interactor)
+    public void Interact()
     {
         if (!justPutDown)
         {
