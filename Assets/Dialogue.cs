6fa354698244f184ed34543b3334a0db7ea1644a
Ciar <138235847+Lucas-van-Dam@users.noreply.github.com>
2023-09-14 14:58:22 +0200

    dialogue revamped

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -4,37 +4,13 @@ using UnityEngine;
 using UnityEngine.Events;
 using Yarn.Unity;
 
-[RequireComponent(typeof(DialogueRunner))]
 public class Dialogue : MonoBehaviour, IInteractable
 {
-
-    [SerializeField] private DialogueRunner dialogue;
     [SerializeField] private string startNodeName;
 
-    private GameObject player;
     public void Interact(GameObject interactor)
     {
-        if (dialogue.IsDialogueRunning)
-            return;
-        dialogue.StartDialogue(startNodeName);
-        player = interactor;
-        player.GetComponent<Movement>()?.Paralyse();
-        dialogue.onDialogueComplete.AddListener(End);
-    }
-
-    [YarnCommand("End")]
-    public static void EndDialogue()
-    {
-        var move = (Movement)FindObjectOfType(typeof(Movement));
-        move.Paralyse();
-        
-    }
-
-    public void End()
-    {
-        Debug.Log("END");
-        player.GetComponent<Movement>()?.Paralyse();
-        
+        DialogueManager.RunDialogue(startNodeName);
     }
 
     [YarnCommand("Dissapear")]
@@ -42,8 +18,7 @@ public class Dialogue : MonoBehaviour, IInteractable
     {
         Destroy(gameObject);
     }
-
-
+    
     public string getToolTipText()
     {
         return "to talk";
