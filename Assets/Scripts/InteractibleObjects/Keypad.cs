42578e1b671c7035bee21ec741c5be3f11eb0176
Ciar <138235847+Lucas-van-Dam@users.noreply.github.com>
2023-09-19 14:58:26 +0200

    Update Keypad.cs

--- Assets/Scripts/InteractibleObjects/Keypad.cs
+++ Assets/Scripts/InteractibleObjects/Keypad.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Keypad : MonoBehaviour, IInteractable
 {
     [SerializeField] private string tooltipText;
-    
     [SerializeField] private GameObject keypadUI;
+    [SerializeField] private AudioClip opening;
     
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,7 @@ public class Keypad : MonoBehaviour, IInteractable
 
     public void Interact(GameObject interactor)
     {
+        GetComponent<AudioSource>().PlayOneShot(opening);
         interactor.GetComponent<Movement>().Paralyse();
         keypadUI.SetActive(true);
         keypadUI.GetComponent<KeypadUI>().Enable(interactor);
